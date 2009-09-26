9a977e0c53305ef0f172734ac6fd238d3accffe7
Sergio Campos <seocam@seocam.net>
2009-09-26 17:12:41 +0200

    Moving scanner serializer to utils

--- imagescanner/core/server.py
+++ imagescanner/core/server.py
@@ -1,23 +1,17 @@
 import cjson
-from imagescanner import ImageScanner
 
 from cStringIO import StringIO
 from twisted.web import server
 from twisted.web.xmlrpc import XMLRPC, Binary
 
-def serialize(device):
-    return {
-        'id': device.id,
-        'name': device.name,
-        'manufacturer': getattr(device, 'manufacturer', None),
-        'description': getattr(device, 'description', None),
-    }
+from imagescanner import ImageScanner
+from imagescanner.utils import scanner_serializer
 
 class ScannerDevices(XMLRPC):
 
     def xmlrpc_list_scanners(self):
         devices = ImageScanner().list_scanners()
-        serialized_devices = [serialize(device) for device in devices]
+        serialized_devices = [scanner_serializer(device) for device in devices]
         return cjson.encode(serialized_devices)
 
     def xmlrpc_scan(self, device_id):
