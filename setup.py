797e2b15c90143cff0944512e747c86508b58d39
Sergio Oliveira <seocam@seocam.com>
2011-02-05 15:05:14 +0100

    Updating my name =)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='imagescanner',
       version='0.9-alpha',
       description='Multi-platform Python library to access scanner devices.',
-      author='Sergio Oliveira Campos',
+      author='Sergio Oliveira',
       author_email='seocam@seocam.com',
       url='http://code.google.com/p/imagescanner/',
       packages=find_packages(),
